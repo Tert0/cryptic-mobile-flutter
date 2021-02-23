1f5b36d3fef4b72ba43faeca518186b574313c87
heatdream <pete.fynn05@gmail.com>
2021-02-23 09:42:28 +0100

    Fixed  CircularProgressIndicator  and Center the Text Widget for  online players

--- lib/home.dart
+++ lib/home.dart
@@ -9,7 +9,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _State extends State<HomeScreen> {
-
   Future<int> online_player() async {
     Client cl = await Client().init('wss://ws.cryptic-game.net');
     int players = (await cl.request({"action": "status"}))['online'];
@@ -19,14 +18,17 @@ class _State extends State<HomeScreen> {
   @override
   Widget build(context) {
     return FutureBuilder<int>(
-      future: online_player(),
-      builder: (context, AsyncSnapshot<int> snapshot) {
-        if (snapshot.hasData) {
-          return Text(snapshot.data.toString());
-        } else {
-          return CircularProgressIndicator();
-        }
-      }
-    );
+        future: online_player(),
+        builder: (context, AsyncSnapshot<int> snapshot) {
+          if (snapshot.hasData) {
+            return Center(child: Text(snapshot.data.toString()));
+          } else {
+            return Center(
+              child: Container(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
   }
 }
